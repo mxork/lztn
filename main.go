2ced039d83785a44f92e8ee395bbb70d56f4ff79
Dan Anderson <dan.hillier.anderson@gmail.com>
2015-07-28 07:03:00 -0400

    no idea

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+var _ = fmt.Println
 var _ = fftw.FFT
 var _ = exec.Command
 var _ = os.Stdin
@@ -29,6 +30,20 @@ func maxmin(b []byte) (max, min int) {
 	return
 }
 
+func maxfreq(nrml []float64) (max, f float64) {
+	max, mi := 0, 0
+	off := 10
+	for i, v := range nrml[off : len(nrml)/2] {
+		if v > max {
+			max = v
+			mi = i
+		}
+	}
+
+	f = float64(mi+off) * SAMP / N
+	return
+}
+
 //dummy 1000hz generator
 type dummy struct {
 	c float64
@@ -44,16 +59,48 @@ func (d dummy) Read(b []byte) (n int, err error) {
 	return len(b), nil
 }
 
-const SAMP = 44100
-const SMPI = 1 / 44100
-const N = 2 << 14
+// actually, modulus squared
+func modulus(x complex64) float64 {
+	return math.Sqrt(float64(real(x * complex(real(x), -imag(x)))))
+}
+
+// toss out the first few, and the top half
+func maxminc(s []complex64) (max, min float64, maxi, mini int) {
+	max, min = 0, math.MaxFloat64
+	for i, x := range s[20 : len(s)/2] {
+		v := modulus(x)
+		if v > max {
+			max, maxi = v, i
+		}
+		if v < min {
+			min, mini = v, i
+		}
+	}
+	return
+}
+
+func binf(i int) float64 {
+	return float64(i) * SAMP / N
+}
+
+func fbin(f float64) int {
+	return int(f * N / SAMP)
+}
+
+var SAMP = 44100.0
+
+const N = 2 << 13
 
 func main() {
-	bf := bufio.NewReaderSize(os.Stdin, SAMP) // one sec
+	bf := bufio.NewReaderSize(os.Stdin, int(SAMP)) // one sec
 	b := make([]byte, N)
 	in := fftw.NewArray(N)
 	out := fftw.NewArray(N)
+	nrml := make([]float64, N)
 	plan := fftw.NewPlan(in, out, fftw.Forward, fftw.DefaultFlag)
+
+	avg := 100.0
+	last := time.Now()
 
 	for _ = range time.Tick(500 * time.Millisecond) {
 		n, err := bf.Read(b)
@@ -66,9 +113,23 @@ func main() {
 		}
 
 		plan.Execute()
+
 		for i, v := range out.Elems {
-			fmt.Println(float64(i)*SAMP/N, real(v*complex(real(v), -imag(v))))
+			nrml[i] = modulus(v) / math.Sqrt(N)
 		}
+
+		sum := 0.0
+		for _, v := range nrml[fbin(1100):fbin(1300)] {
+			sum += v
+		}
+		if sum > 10*avg && time.Since(last).Seconds() > 5 {
+			fmt.Println(sum, avg)
+			if len(os.Args) == 2 {
+				exec.Command(os.Args[1]).Run()
+			}
+		}
+
+		avg = (avg + sum) / 2
 
 		bf.Reset(os.Stdin)
 	}
